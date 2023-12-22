fd0058e55cf0286ff8945bfed0b18d7f958328a5
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-12-22 19:14:29 +0600

    app create successfull

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    // eslint-disable-next-line no-console
+    console.log("Connected to the database");
+
+    app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Error connecting to the database:", error);
+  }
+}
+main();
